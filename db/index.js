6194f82de1bce60297d792d3ba2f3665d444c688
chriscauley <chris@lablackey.com>
2019-01-15 02:29:26 +0000

    flushing out app and adding to uR.db.apps

--- db/index.js
+++ db/index.js
@@ -2,14 +2,22 @@ import Ready from '../ready'
 import Manager from "./Manager"
 import Model from "./Model"
 import * as fields from "./fields"
+import unslugify from '../schema/unslugify'
 
 const register = model => {
   const { app_label, model_name } = model
   if (!db[app_label]) {
-    db[app_label] = {}
+    db[app_label] = {
+      verbose_name: unslugify(app_label),
+      name: app_label,
+      _models: [],
+    }
+    db.apps.push(db[app_label])
   }
   db[app_label][model_name] = model
+  db[app_label]._models.push(model)
   db[`${app_label}.${model_name}`] = model
+  model.verbose_name = model.verbose_name || unslugify(model_name)
 }
 
 const db = {
@@ -18,6 +26,7 @@ const db = {
   Manager,
   Model,
   fields,
+  apps: [],
   ...fields,
 }
 
