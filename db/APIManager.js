17d83655396a3a55470333db34d1d51257cb6e9f
chriscauley <chris@lablackey.com>
2019-01-22 01:24:39 +0000

    serialize foreign keys before saving

--- db/APIManager.js
+++ db/APIManager.js
@@ -3,22 +3,35 @@ import db from './index'
 import BaseManager from './BaseManager'
 
 export default class Manager extends BaseManager {
-  create = data => ajax({
-    url: this._get_base_url(),
-    method: 'POST',
-    data: data,
-  }).then(this.set)
+  create = data => {
+    // #! TODO should be db.ready.block or db.ready.ajax
+    db.ready.stop()
+    Object.entries(data).forEach(([key,value]) => {
+      // this is just for foreign key
+      // maybe the Model should constructor could serialize it first?
+      if (value instanceof db.Model) {
+        data[key] = value.id
+      }
+    })
+    return ajax({
+      url: this._get_base_url(),
+      method: 'POST',
+      data: data,
+    })
+      .then(this.set)
+      .then(db.ready.start) //#! TODO
+  }
 
   refresh() {
     // #! TODO should be db.ready.block or db.ready.ajax
     db.ready.stop()
-    ajax(this._get_base_url())
+    return ajax(this._get_base_url())
       .then(response => {
         this.pagination = response.pagination
         this.items = new Map()
         response.results.forEach(this.set)
       })
-      .then(db.ready.start)
+      .then(db.ready.start) //#! TODO
   }
 
   _get_base_url() {
