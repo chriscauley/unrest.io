b56df204368ffa66c6b7c8a937b4aac0bed65205
chriscauley <chris@lablackey.com>
2019-01-24 02:21:11 +0000

    Model.slug can now be used in place of app_label,model_name

--- db/index.js
+++ db/index.js
@@ -3,13 +3,19 @@ import APIManager from "./APIManager"
 import BaseManager from "./BaseManager"
 import Model from "./Model"
 import * as fields from "./fields"
-import unslugify from '../schema/unslugify'
+import unslugify, { camel2SpaceCase } from '../schema/unslugify'
 
-const register = model => {
-  const { app_label, model_name } = model
-  if (!model_name || !app_label) {
-    throw "Model needs app_label and model_name"
+const _nameAppAndModel = model => {
+  if (model.slug) {
+    [model.app_label, model.model_name] =  model.slug.split(".")
+  } else if (model.app_label && model.model_name) {
+    model.slug = `${model.app_label}.${model.model_name}`
+  } else {
+    throw `Model "${model.name}" needs app_label and model_name or a slug`
   }
+}
+
+const _getOrCreateApp = app_label => {
   if (!db[app_label]) {
     db[app_label] = {
       verbose_name: unslugify(app_label || "main"),
@@ -18,10 +24,21 @@ const register = model => {
     }
     db.apps.push(db[app_label])
   }
-  db[app_label][model_name] = model
-  db[app_label]._models.push(model)
-  db[`${app_label}.${model_name}`] = model
-  model.verbose_name = model.verbose_name || unslugify(model_name)
+  return db[app_label]
+}
+
+const register = model => {
+  _nameAppAndModel(model)
+
+  let { app_label, model_name, slug } = model
+  const app = _getOrCreateApp(app_label)
+
+  // assign model to app and uR.db
+  db[slug] = app[model_name] = model
+  app._models.push(model)
+
+  // human readable string e.g. "BlogPost" => "Blog Post"
+  model.verbose_name = model.verbose_name || camel2SpaceCase(model_name)
 }
 
 const db = {
