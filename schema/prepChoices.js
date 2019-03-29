84b9c6ad0bef136f823e97bbb4ce2130471b8bc6
chriscauley <chris@lablackey.com>
2019-03-29 11:39:20 +0000

    tests for foreign key and datetime

--- schema/prepChoices.js
+++ schema/prepChoices.js
@@ -1,6 +1,9 @@
 import parseChoices from './parseChoices'
 
 export default opts => {
+  if (typeof opts === "function") {
+    opts = opts()
+  }
   return parseChoices(opts.choices).map((c, index) => ({
     value: c[0],
     label: c[1],
--- tests/TodoTests.js
+++ tests/TodoTests.js
@@ -1,8 +1,19 @@
 import _ from 'lodash'
+import { expect } from 'chai'
+import df from 'date-fns'
 
 import uR from '../'
 const { Snap } = uR
-const { Model, Int, String, Boolean, StorageManager, ForeignKey } = uR.db
+
+const {
+  Boolean,
+  DateTime,
+  ForeignKey,
+  Int,
+  Model,
+  String,
+  StorageManager,
+} = uR.db
 
 class BaseModel extends Model {
   static manager = StorageManager
@@ -17,6 +28,7 @@ class Todo extends BaseModel {
     name: String(),
     completed: Boolean(false),
     project: ForeignKey('test.Project'),
+    due: DateTime({required: false}),
   }
 }
 
@@ -34,8 +46,7 @@ export default () => {
   it('TODO: can be marked completed', function(done) {
     const { title } = this.test
     const snap = Snap(this)
-    Todo.objects
-      .create({
+    Todo.objects.create({
         name: title,
       })
       .then(obj => {
@@ -49,5 +60,53 @@ export default () => {
         snap.match('complete', obj.serialize())
         snap.done(done)
       })
+  })
+
+  it("PROJECT: can be saved as fk on Todo", async function (done) {
+    const { title } = this.test
+    const snap = Snap(this)
+    const project = await Project.objects.create({name:title})
+    snap.match("project",project.serialize())
+    const todo = await Todo.objects.create({
+      name: title,
+      project,
+    })
+    snap.match("todo with project",todo.serialize())
+    expect(project.id).to.equal(todo.serialize().project)
+    expect(project).to.equal(todo.project)
+    Todo.objects.refresh()
+    const same_todo = Todo.objects.get(todo.id)
+    snap.match("todo with project",same_todo.serialize())
+
+    done()
+  })
+
+  it("TODO: can save due date", async function(done) {
+    const dt_string = "2019-01-01 00:00:00"
+    const dt = new Date(dt_string)
+    const todoWithDue = due => Todo.objects.create({
+      name: title,
+      due,
+    })
+    const { title } = this.test
+    const snap = Snap(this)
+    todoWithDue(dt).then(todo => {
+      snap.match("todo with 1/1/19 due date",todo.serialize())
+    })
+
+    Promise.all([
+      todoWithDue(dt_string),
+      todoWithDue(dt),
+      todoWithDue(dt.valueOf()),
+    ]).then( todos => done(
+      todos.map(todo => {
+        if (!df.isEqual(todo.due,dt)) {
+          return `Live value is not equal to instantiated value`
+        }
+        if (!df.isEqual(todo.serialize().due,dt)) {
+          return `Serialized value is not equal to instantiated value`
+        }
+      }).find(err=>err)
+    ))
   })
 }
